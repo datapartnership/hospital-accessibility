4c9286f2793aeb551f3b7279b122794a9b1ce235
MrMaksimize <max@maksimize.com>
2020-09-02 15:40:06 -0400

    Add .env.

--- run_env.sh
+++ run_env.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 echo "Running Container"
+
+source ./.env
+
+echo $MAPBOX_TOKEN
+
 docker run -p 8888:8888 \
     -v "$PWD":/home/jovyan/work \
     --env GRANT_SUDO=yes \
     --env JUPYTER_ENABLE_LAB=yes \
     --env RESTARTABLE=yes \
+    --env MAPBOX_TOKEN=$MAPBOX_TOKEN \
     --rm \
     mrmaksimize/hospital-access-env:latest start-notebook.sh 
 
